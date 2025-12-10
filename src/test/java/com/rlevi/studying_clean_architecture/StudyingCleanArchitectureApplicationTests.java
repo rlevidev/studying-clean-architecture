c33b2c6c4c8a2112f3fe5c3ac272d01c3855f600
rlevidev <leviizinho765@gmail.com>
2025-12-09 21:57:09 -0300

    ✨ feat(projeto): Configura a estrutura inicial do projeto Spring Boot com Maven

---
+++ src/test/java/com/rlevi/studying_clean_architecture/StudyingCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlevi.studying_clean_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingCleanArchitectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
