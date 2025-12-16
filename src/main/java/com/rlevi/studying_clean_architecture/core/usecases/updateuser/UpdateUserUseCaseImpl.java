2514a801cbca3aac72fd087a958024c423f215e9
rlevidev <leviizinho765@gmail.com>
2025-12-16 19:18:37 -0300

    ✨ feat(UpdateUserUseCase): Implementa codificação de senha para atualização de usuários
    - Verificar se uma nova senha foi fornecida.
    
    - Encriptar a nova senha usando PasswordEncoder.

--- src/main/java/com/rlevi/studying_clean_architecture/core/usecases/updateuser/UpdateUserUseCaseImpl.java
+++ src/main/java/com/rlevi/studying_clean_architecture/core/usecases/updateuser/UpdateUserUseCaseImpl.java
@@ -2,12 +2,16 @@ package com.rlevi.studying_clean_architecture.core.usecases.updateuser;
 
 import com.rlevi.studying_clean_architecture.core.entities.User;
 import com.rlevi.studying_clean_architecture.core.gateway.UserGateway;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
   private final UserGateway userGateway;
+  private final PasswordEncoder passwordEncoder;
 
-  public UpdateUserUseCaseImpl(UserGateway userGateway) {
+
+  public UpdateUserUseCaseImpl(UserGateway userGateway, PasswordEncoder passwordEncoder) {
     this.userGateway = userGateway;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Override
@@ -21,6 +25,19 @@ public class UpdateUserUseCaseImpl implements UpdateUserUseCase {
     var existingUser = userGateway.findUserById(user.id())
             .orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + user.id()));
 
-    return userGateway.updateUser(user);
+    String passwordHash = existingUser.passwordHash();
+    if (user.passwordHash() != null && !user.passwordHash().isBlank()) {
+      passwordHash = passwordEncoder.encode(user.passwordHash());
+    }
+
+    User updatedUser = new User(
+            existingUser.id(),
+            user.email(),
+            user.name(),
+            passwordHash,
+            user.role()
+    );
+
+    return userGateway.updateUser(updatedUser);
   }
 }
