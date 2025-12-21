834076435bc1875d4c4ae9078894fca244bbc612
rlevidev <rlevi.dev@gmail.com>
2025-12-21 19:05:15 -0300

    ✨ feat(security): Implementar service de detalhes do usuário para autenticação

---
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.rlevi.studying_clean_architecture.infrastructure.security;
+
+import com.rlevi.studying_clean_architecture.core.entities.User;
+import com.rlevi.studying_clean_architecture.core.gateway.UserGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+  @Autowired
+  private UserGateway userGateway;
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userGateway.findUserByEmail(email)
+            .orElseThrow(() ->
+                    new UsernameNotFoundException("User not found with email: " + email)
+            );
+
+    return org.springframework.security.core.userdetails.User.builder()
+            .username(user.email())
+            .password(user.passwordHash())
+            .authorities("ROLE_" + user.role().name())
+            .build();
+  }
+}
