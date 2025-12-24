88f9c1ba8460c905b2611b96f4d21b7e079c1602
rlevidev <rlevi.dev@gmail.com>
2025-12-24 16:08:00 -0300

    ⚙️ chore(security): Registra CustomAuthenticationEntryPoint como um componente Spring
    # Esta mudança é essencial para que o Spring Security reconheça e utilize a implementação personalizada de `AuthenticationEntryPoint` para tratar falhas de autenticação.
    
    # Componentes Principais:
     - CustomAuthenticationEntryPoint: Define o comportamento para lidar com exceções de autenticação, retornando uma resposta JSON padronizada para clientes não autenticados.
    
    # Fluxo:
    
     - Fluxo de Autenticação Falha:
      Requisição Não Autenticada → Filtro de Segurança → CustomAuthenticationEntryPoint.commence() → Resposta HTTP 401 JSON

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/exception/CustomAuthenticationEntryPoint.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
