bab5f8c9aeec82068e74e990dd082f00c3d462b0
rlevidev <rlevi.dev@gmail.com>
2026-01-28 19:07:38 -0300

    ✨ feat(openapi): Adicionar configuração do OpenAPI para documentação da API
    # Propósito:
    Este commit introduz a configuração do OpenAPI (Swagger) para gerar a documentação interativa da API.
    
    # Componentes Principais:
     - OpenApiConfig: Define o bean OpenAPI para a documentação da API, incluindo metadados e esquema de segurança JWT.
    
    # Fluxo:
     - Inicialização do Spring Boot:
      Spring Boot → OpenApiConfig → Bean `OpenAPI` configurado e disponível para o SpringDoc.

---
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.rlevi.studying_clean_architecture.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+
+public class OpenApiConfig {
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .info(new Info()
+                    .title("Studying Clean Architecture API")
+                    .version("0.0.1")
+                    .description("API for studying Clean Architecture")
+                    .contact(new Contact()
+                            .name("R. Levi")
+                            .email("rlevi.dev@gmail.com"))
+                    .license(new License()
+                            .name("Apache 2.0")
+                            .url("http://springdoc.org")))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components()
+                    .addSecuritySchemes("bearerAuth",
+                            new SecurityScheme()
+                                    .name("bearerAuth")
+                                    .type(SecurityScheme.Type.HTTP)
+                                    .scheme("bearer")
+                                    .bearerFormat("JWT")));
+  }
+}
