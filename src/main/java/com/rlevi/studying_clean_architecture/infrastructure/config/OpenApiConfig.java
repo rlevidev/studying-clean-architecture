7cdf186dcfd452aac3f0c0e98b34ee7a80da12dc
rlevidev <rlevi.dev@gmail.com>
2026-01-29 20:59:20 -0300

    ✨ feat(config): Marcar OpenApiConfig como classe de configuração Spring
    # Este commit garante que a classe OpenApiConfig seja explicitamente reconhecida pelo Spring
    # como uma fonte de definições de beans, permitindo que a configuração do OpenAPI
    # seja carregada corretamente no contexto da aplicação.
    
    # Componentes Principais:
     - OpenApiConfig: Contém a definição da configuração personalizada para o Swagger/OpenAPI.
     - @Configuration: Anotação que designa uma classe como fonte de definições de beans para o Spring IoC container.
    
    # Fluxo:
    
     - Inicialização da Aplicação Spring:
      Início da Aplicação → Spring Component Scan → Identifica OpenApiConfig como @Configuration → Processa métodos @Bean (customOpenAPI) → Registra o bean OpenAPI no contexto da aplicação.

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/config/OpenApiConfig.java
@@ -8,7 +8,9 @@ import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OpenApiConfig {
   @Bean
   public OpenAPI customOpenAPI() {
