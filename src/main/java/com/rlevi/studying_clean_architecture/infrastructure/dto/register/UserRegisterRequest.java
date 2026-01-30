a3c05e01589617acefa915de5efbcac8c5a3c570
rlevidev <rlevi.dev@gmail.com>
2026-01-30 19:55:45 -0300

    📝 docs(register): Adicionar documentação Swagger para UserRegisterRequest
    # Este commit adiciona anotações Swagger/OpenAPI ao DTO de registro de usuário para melhorar a clareza e a completude da documentação da API.
    
    # Componentes Principais:
     - UserRegisterRequest: Define a estrutura de dados para solicitações de registro de usuário, incluindo campos como nome, email e senha.
     - Schema (Swagger/OpenAPI): Fornece metadados para descrever os campos do DTO na documentação gerada, adicionando descrições e exemplos.
    
    # Fluxo:
    
     - [Documentação da API]:
      DTO UserRegisterRequest → Anotações @Schema → Geração da Especificação OpenAPI → Visualização no Swagger UI

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/register/UserRegisterRequest.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/register/UserRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.rlevi.studying_clean_architecture.infrastructure.dto.register;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -7,14 +8,17 @@ import jakarta.validation.constraints.Size;
 public record UserRegisterRequest(
         @NotBlank(message = "Name is required.")
         @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters.")
+        @Schema(description = "User's name", example = "Dexter Morgan")
         String name,
 
         @NotBlank(message = "Email is required.")
         @Email(message = "Invalid email format.")
+        @Schema(description = "User's email", example = "dextermorgan@serialkiller.com")
         String email,
 
         @NotBlank(message = "Password is required.")
         @Size(min = 8, message = "Password must have at least 8 characters.")
+        @Schema(description = "User's password", example = "password123")
         String password
 ) {
 }
