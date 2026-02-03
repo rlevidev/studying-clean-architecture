9b3d9afa271dce4a6a8d002f0f453a296f94cfac
rlevidev <rlevi.dev@gmail.com>
2026-02-03 17:41:29 -0300

    📚 docs(register): Adicionar anotações Swagger para UserRegisterResponse
    # Esta mudança aprimora a documentação da API para a resposta de registro de usuário, tornando-a mais clara e completa no Swagger UI.
    
    # Componentes Principais:
     - `UserRegisterResponse`: Um DTO (Data Transfer Object) que representa a resposta de sucesso após o registro de um usuário, incluindo mensagem, token de acesso e refresh token.
     - `@Schema Annotation`: Uma anotação do Swagger (OpenAPI) utilizada para fornecer descrições detalhadas e exemplos para os campos de um modelo DTO, melhorando a documentação automática da API.
    
    # Fluxo:
    
     - [Geração de Documentação da API]:
      Definição do DTO → Aplicação de `@Schema` → Geração Automática de Docs (Swagger) → Visualização no Swagger UI

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/register/UserRegisterResponse.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/register/UserRegisterResponse.java
@@ -1,8 +1,15 @@
 package com.rlevi.studying_clean_architecture.infrastructure.dto.register;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserRegisterResponse(
+        @Schema(description = "Success message", example = "User registered successfully.")
         String message,
+
+        @Schema(description = "JWT Access Token used for authenticating requests", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
         String token,
+
+        @Schema(description = "Refresh token used to obtain a new access token", example = "a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6")
         String refreshToken
 ) {
   public static UserRegisterResponse success(String token, String refreshToken) {
