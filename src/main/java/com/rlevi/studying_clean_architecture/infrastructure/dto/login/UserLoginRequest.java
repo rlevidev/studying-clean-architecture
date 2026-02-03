9153be5f1b04ff57dca949a69876b92f704bbe49
rlevidev <rlevi.dev@gmail.com>
2026-02-03 17:37:30 -0300

    📝 docs(login): Adicionar anotações Swagger para UserLoginRequest
    # Este commit adiciona anotações `@Schema` aos campos do DTO `UserLoginRequest` para aprimorar a documentação gerada automaticamente da API.
    
    # Componentes Principais:
     - UserLoginRequest: Um Data Transfer Object (DTO) que representa os dados de requisição para o endpoint de login.
     - @Schema: Anotações do Swagger que fornecem metadados (descrição, exemplo) para os campos do DTO, melhorando a clareza da documentação da API.
    
    # Fluxo:
    
     - Documentação da API:
      DTO `UserLoginRequest` → Anotações `@Schema` → Ferramenta de Geração de Docs (ex: Swagger UI) → Documentação de API Aprimorada

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/login/UserLoginRequest.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/login/UserLoginRequest.java
@@ -1,7 +1,12 @@
 package com.rlevi.studying_clean_architecture.infrastructure.dto.login;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserLoginRequest(
+        @Schema(description = "User's email", example = "dextermorgan@serialkiller.com")
         String email,
+
+        @Schema(description = "User's password", example = "password123")
         String password
 ) {
 }
