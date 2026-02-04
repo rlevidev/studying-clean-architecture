e0c0ac53adfbac9c4f5e5396739c3f4e75c2266d
rlevidev <rlevi.dev@gmail.com>
2026-02-04 15:17:54 -0300

    feat(swagger): Adiciona anotações ao DTO RefreshTokenResponse
    Este commit adiciona anotações @Schema ao DTO RefreshTokenResponse.
    Elas fornecem descrições e exemplos para os campos 'message',
    'accessToken' e 'refreshToken', melhorando a documentação da API.

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/refreshtoken/RefreshTokenResponse.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/refreshtoken/RefreshTokenResponse.java
@@ -1,8 +1,16 @@
 package com.rlevi.studying_clean_architecture.infrastructure.dto.refreshtoken;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Request to obtain a new access token using a refresh token")
 public record RefreshTokenResponse(
+        @Schema(description = "Success message", example = "Token refreshed successfully.")
         String message,
+
+        @Schema(description = "JWT Access Token used for authenticating requests", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
         String accessToken,
+
+        @Schema(description = "Refresh token used to obtain a new access token", example = "a1b2c3d4-e5f6-7g8h-9i0j-k1l2m3n4o5p6")
         String refreshToken
 ) {
   public static RefreshTokenResponse success(String accessToken, String refreshToken) {
