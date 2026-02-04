332d966815e880a23cd6b8e68906b78c7daf8b25
rlevidev <rlevi.dev@gmail.com>
2026-02-04 15:12:29 -0300

    feat(swagger): Documenta DTO de login com anotação Schema
    Adiciona a anotação '@Schema' ao record 'UserLoginRequest'
    para fornecer uma descrição clara do DTO na documentação
    do Swagger.

--- src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/login/UserLoginRequest.java
+++ src/main/java/com/rlevi/studying_clean_architecture/infrastructure/dto/login/UserLoginRequest.java
@@ -2,6 +2,7 @@ package com.rlevi.studying_clean_architecture.infrastructure.dto.login;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Request to login a user")
 public record UserLoginRequest(
         @Schema(description = "User's email", example = "dextermorgan@serialkiller.com")
         String email,
